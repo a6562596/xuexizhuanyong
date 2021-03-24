da5c20ca7126849ca60c561b847bcf94b54e2592
runthe <654424350@qq.com>
2021-03-24 19:40:59 +0800

    3.24学习

--- pythonProject02/01.black.py
+++ pythonProject02/01.black.py
@@ -1,2 +1,11 @@
-# 清除空白
-print("1234")
+favorite_language = ' python '
+print(favorite_language)
+
+# 删除后面的空格
+print(favorite_language.rstrip())
+
+# 删除前面的空格
+print(favorite_language.lstrip())
+
+# 删除前后的空格
+print(favorite_language.strip())
