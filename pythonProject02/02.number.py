792729bbd343adb98010e74aed6578696fcedbc9
runthe <654424350@qq.com>
2021-03-24 21:27:05 +0800

    3.24学习

---
+++ pythonProject02/02.number.py
@@ -0,0 +1,21 @@
+# 加减乘除
+sum1 = (2 + 3) / 2
+print(sum1)
+
+# 3的乘方
+chengfang = 3 ** 3
+print(chengfang)
+
+
+# 浮点数
+fudian = 1.2 + 1.3
+print(fudian)
+
+fudian2 = 3 * 0.1
+print(fudian2)
+
+
+# 使用函数str() 避免类型错误,把一个int类型的数字转化为字符串
+age = 23
+message = "Happy " + str(age) + "rd Birthday"
+print(message)
