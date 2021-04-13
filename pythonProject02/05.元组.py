45205d63f3212732d30696c9aee307c5c39924c4
runthe <654424350@qq.com>
2021-04-13 15:49:48 +0800

    4.13学习

--- pythonProject02/05.元组.py
+++ pythonProject02/05.元组.py
@@ -1,1 +1,12 @@
 # 元组里面的元素是不可修改的
+foods = ('皮皮虾','橙子','冰淇淋','牛排','可乐')
+for food in foods:
+    print(food)
+
+print()
+#foods[0] = '小龙虾'
+#print(foods)
+
+foods = ('小龙虾','橙子','牛肚','牛排','可乐') # 用变量赋值的方式来修改元组
+for food2 in foods:
+    print(food2)
